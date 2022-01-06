416b681d795cddeacf4f6046d3f6ac1d72dadcde
Himanish Mandrekar <mandrekarh25@gmail.com>
2022-01-06 17:41:28 +0530

    bringup for conquerOS

--- conquer_laurel_sprout.mk
+++ conquer_laurel_sprout.mk
@@ -11,31 +11,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-ifeq (aospa_laurel_sprout,$(TARGET_PRODUCT))
-
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit from the custom device configuration.
-$(call inherit-product, device/xiaomi/laurel_sprout/device.mk)
+# Inherit from violet device
+$(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit from the AOSPA configuration.
-$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
-
-PRODUCT_BRAND := Xiaomi
-PRODUCT_DEVICE := laurel_sprout
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Mi A3
-PRODUCT_NAME := aospa_laurel_sprout
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Override device name for Play Store.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=Mi A3
-
-# Boot animation resolution.
+# Inherit some common ConquerOS stuff.
+$(call inherit-product, vendor/conquer/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 720
+TARGET_GAPPS_ARCH := arm64
 
-endif
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := conquer_laurel_sprout
+PRODUCT_DEVICE := laurel_sprout
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi A3
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR_PRODUCT_NAME := laurel_sprout
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
