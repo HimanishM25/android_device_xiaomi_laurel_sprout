5452ac1c4f6a74903114284e7896710c0676c363
Himanish Mandrekar <mandrekarh25@gmail.com>
2022-01-12 17:48:42 +0530

    Revert "Track udfps changes from Xiaomi Raphael"
    This reverts commit 8db5dcb20bdd2911375b71e37b11b1807db7f31a.

--- device.mk
+++ device.mk
@@ -55,8 +55,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
 # Biometrics
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml 
+
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_laurel_sprout
+    android.hardware.biometrics.fingerprint@2.3-service.laurel_sprout \
+    libvendor.goodix.hardware.biometrics.fingerprint@2.1 \
+    vendor.xiaomi.hardware.fingerprintextension@1.0
 
 # Bluetooth
 PRODUCT_PACKAGES += \
